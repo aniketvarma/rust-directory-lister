fcade7f6885e7db778ad3bca99d8d1d7a877da3f
Aniket Varma <aniketvarma@outlook.com>
2025-12-23 21:35:03 +0530

    Add multiple path support and refactor into helper function

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,52 @@ use std::fs;
 use std::io::{BufWriter, Write};
 
 fn main() {
-    println!("Hello, world!");
+
+  // Collect command-line arguments
     let arguments: Vec<String> = std::env::args().collect();
 
-    let path = if arguments.len() == 1 {
-        String::from(".")
-    } else {
-        arguments[1].clone()
-    };
+   // If there are multiple arguments, list contents for each specified path
+    if arguments.len() != 1 {
+        for arg in arguments.iter().skip(1){
+            println!("{}:", arg);
+            list_contents(arg);
+            println!("");
+        }
+        // If no arguments are provided, list contents of the current directory
+    }else{
+        list_contents(".");
+        
+    }
     
+}
+
+ /// Function to list contents of a directory
+fn list_contents(path: &str) {
+
+    // handle to standard output
     let stdout = std::io::stdout();
+
+    // buffered writer which store all the contents before printing them to the console
     let mut handle = BufWriter::new(stdout.lock());
 
-    match fs::read_dir(path) {
+     match fs::read_dir(path) {
+
+        //read_dir gives an iterator over the entries within the directory
         Ok(entries) => {
+
+            //using for loop to iterate over each entry in the directory
             for entry in entries {
                 match entry {
                     Ok(dir_entry) => {
+
+                        // Get the file type (directory or file)
                         let file_type = dir_entry.file_type().unwrap();
+
+                        //check if the entry is a directory or a file and write to the handle accordingly
                         if file_type.is_dir() {
+
+                            // Append a '/' to directory names, to distinguish them from files
+                            // .to_string_lossy() converts OsString to String for display
                             writeln!(handle, "{}/", dir_entry.file_name().to_string_lossy())
                                 .unwrap();
                         } else if file_type.is_file() {
